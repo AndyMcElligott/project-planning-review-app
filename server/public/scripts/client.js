789c6b7c08427f8bc3dc6c417b0f5e6f0635f1de
AndyMcElligott <andymc2315@gmail.com>
2020-01-06 11:00:25 -0600

    input fields working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,33 @@
+/* eslint-disable object-shorthand */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
 $(document).ready(onReady);
 
 function onReady() {
   console.log('in onReady');
+  $('.inputDate').empty();
+  $('.snowAmt').empty();
+  $('.submitBtn').on('click', dataToAdd);
+}
+
+function dataToAdd() {
+  // test to see if function is being called on click
+  console.log('in dataToAdd');
+  let dateInput = $('.inputDate').val();
+  let snowInput = $('.snowAmt').val();
+  // test to see if inputs were gathered
+  console.log(dateInput, snowInput);
+
+  // identify data to send
+  let dataToSend = {
+    dateInput: dateInput,
+    snowInput: snowInput,
+  };
+  console.log('dataToSend', dataToSend);
+
+  // empty out user inputs
+  $('.inputDate').val('');
+  $('.snowAmt').val('');
 }
